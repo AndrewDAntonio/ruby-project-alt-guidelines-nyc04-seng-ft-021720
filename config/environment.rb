768ece4b03b8d2daab51e243cdd16796d7c25a49
erocknine <echeungsk@gmail.com>
2020-03-03 14:05:39 -0500

    Revised all migrations to -create

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,3 @@
-# require_relative '../app/models/service.rb'
-
 require 'bundler'
 Bundler.require
 
--- db/migrate/01_create_services.rb
+++ db/migrate/01_create_services.rb
@@ -1,4 +1,4 @@
-class Service < ActiveRecord::Migration[5.2]
+class CreateServices < ActiveRecord::Migration[5.2]
   def change
     create_table :services do |t|
       t.string :name
