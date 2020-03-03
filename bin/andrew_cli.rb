78790dbe560372a64f4554f00906b9200b2a1159
AndrewDAntonio <github email address>
2020-03-03 16:02:33 -0500

    Welcome and menu methods

--- bin/andrew_cli.rb
+++ bin/andrew_cli.rb
@@ -1,5 +1,11 @@
+require 'bundler'
+Bundler.require
+
+
 require_relative '../config/environment'
+require_all 'app'
 require 'tty-prompt'
+
 
 
 def welcome
@@ -11,17 +17,24 @@ def shows_or_services
     answer = prompt.select("Where would you like to start?") do |menu|
         menu.choice name: 'Shows', value: 1
         menu.choice name: 'Streaming Services', value: 2
+        menu.choice name: 'Quick Search', value: 3
     end
-    if answer = 1
+    if answer == 1
         show_options
-    else
+    elsif answer == 2
         puts "Still finishing this part" #Streaming Services conditional
+    elsif answer == 3
+        puts "Still finishing this part"
+    else
+        puts "Thanks for visiting Boxguide!"
     end 
 end
 
 def show_options
     prompt = TTY::Prompt.new
     answer = prompt.ask("Type in the name of the show you would like to learn more about.")
+    user_show_choice = Show.find_like(answer)
+    user_show_choice
 
 end
 
